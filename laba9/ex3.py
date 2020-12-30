065d5e1a5950247ce18dce92835f1ce30df141bc
Vita-ultrarus <71723891+Vita-ultrarus@users.noreply.github.com>
2020-12-30 14:17:36 +0300

    Add files via upload

---
+++ laba9/ex3.py
@@ -0,0 +1,36 @@
+import zipfile
+import os
+import string
+from pathlib import Path
+
+
+class TextLoader:
+
+    adr = None
+
+    def __init__(self, adr):
+        self.adr = adr
+
+    def __len__(self):
+        files = os.listdir(self.adr)
+        self.files = files
+        return len(files)
+
+    def __next__(self):
+        zn = ['/', '.', ';', ':', '(', ')', ',', '?', '!']
+        for i in self.files:
+            with open(self.adr + '\\' + i, 'rt') as j:
+                j = str(j)
+                for t in range(len(zn)):
+                    j = j.lower().replace(zn[t], ' ')
+                    self.normal = j
+
+
+    def __iter__(self):
+        return self
+
+
+way = 'C:\\Users\\Имя\\python3sem\\лаба9\\sample'
+a = TextLoader(way)
+print(len(a))
+print(next(a))
